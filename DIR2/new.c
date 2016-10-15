09dd50d6c078857b0d776fd2ad9f71a86308db73
codehimanshu <himanshuagrawal1998@gmail.com>
2016-10-15 21:51:30 +0530

    add files for edit

---
+++ DIR2/new.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void sum(int a, int b)
+{
+
+}
+
+void diff(int a, int b)
+{
+
+}
+
+void pro(int a, int b)
+{
+
+}
+
+void div(int a, int b)
+{
+
+}
+
+void main()
+{
+	int a,b,c;
+	sum(a,b);
+	diff(a,b);
+	pro(a,b);
+	div(a,b);
+}
